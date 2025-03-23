800859e703380ab0a5a43f0ca793959f3bc752bf
Mahito TANNO <mahito@presche.me>
2025-03-23 19:16:59 +0900

    feat:  set status code 404 for non-existing contacts

--- src/routes/contacts/[id].tsx
+++ src/routes/contacts/[id].tsx
@@ -1,10 +1,15 @@
 import { createAsync, query, useParams, type RouteDefinition } from "@solidjs/router";
-import { Show } from "solid-js";
+import { HttpStatusCode } from "@solidjs/start";
+import { ErrorBoundary, Show } from "solid-js";
 import { type ContactRecord, getContact } from "~/data";
 
 const queryContact = query(async (id: string) => {
   "use server";
-  return await getContact(id);
+  const contact = await getContact(id);
+  if (contact == null) {
+    throw new Error("Contact not found");
+  }
+  return contact;
 }, "contactById");
 
 export const route = {
@@ -17,42 +22,51 @@ const Contact = () => {
 
   return (
     <div id="contact">
-      <div>
-        <img alt={`${contact()?.first} ${contact()?.last} avatar`} src={contact()?.avatar} />
-      </div>
-      <div>
-        <h1>
-          <Show when={contact()?.first || contact()?.last} fallback={<i>No Name</i>}>
-            {contact()?.first} {contact()?.last}
+      <ErrorBoundary
+        fallback={(e: Error) => (
+          <Show when={e.message === "Contact not found"}>
+            <HttpStatusCode code={404} />
+            <h1>Contact not found</h1>
           </Show>
-          <Favorite contact={contact() || undefined} />
-        </h1>
-
-        <Show when={contact()?.twitter}>
-          <p>
-            <a href={`https://twitter.com/${contact()?.twitter}`}>{contact()?.twitter}</a>
-          </p>
-        </Show>
-
-        <Show when={contact()?.notes}>
-          <p>{contact()?.notes}</p>
-        </Show>
-
+        )}
+      >
         <div>
-          <form>
-            <button type="submit">Edit</button>
-          </form>
-
-          <form
-            onSubmit={(e) => {
-              const confirmed = confirm("Please confirm you want to delete this record.");
-              if (!confirmed) e.preventDefault();
-            }}
-          >
-            <button type="submit">Delete</button>
-          </form>
+          <img alt={`${contact()?.first} ${contact()?.last} avatar`} src={contact()?.avatar} />
         </div>
-      </div>
+        <div>
+          <h1>
+            <Show when={contact()?.first || contact()?.last} fallback={<i>No Name</i>}>
+              {contact()?.first} {contact()?.last}
+            </Show>
+            <Favorite contact={contact()} />
+          </h1>
+
+          <Show when={contact()?.twitter}>
+            <p>
+              <a href={`https://twitter.com/${contact()?.twitter}`}>{contact()?.twitter}</a>
+            </p>
+          </Show>
+
+          <Show when={contact()?.notes}>
+            <p>{contact()?.notes}</p>
+          </Show>
+
+          <div>
+            <form>
+              <button type="submit">Edit</button>
+            </form>
+
+            <form
+              onSubmit={(e) => {
+                const confirmed = confirm("Please confirm you want to delete this record.");
+                if (!confirmed) e.preventDefault();
+              }}
+            >
+              <button type="submit">Delete</button>
+            </form>
+          </div>
+        </div>
+      </ErrorBoundary>
     </div>
   );
 };
